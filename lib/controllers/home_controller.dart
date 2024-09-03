9f259baddf59c974e39f1ab8310e6e2ac95da829
Dwaipayan <biswastatay73@gmail.com>
2024-09-03 08:13:49 +0530

    Getting some new errors

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,70 @@
-import 'dart:convert';
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import '../helper/my_dialog.dart';
+// import '../helper/pref.dart';
+// import '../models/vpn.dart';
+// import '../models/vpn_config.dart';
+// import '../services/vpn_engine.dart';
+//
+// class HomeController extends GetxController {
+//   final Rx<Vpn> vpn = Pref.vpn.obs;
+//   final vpnState = VpnEngine.vpnDisconnected.obs;
+//
+//   void connectToVpn() async {
+//     if (vpn.value.openVPNConfigDataBase64.isEmpty) {
+//       MyDialogs.info(msg: 'Select a Location by clicking \'Change Location\'');
+//       return;
+//     }
+//
+//     if (vpnState.value == VpnEngine.vpnDisconnected) {
+//       final data = base64.decode(vpn.value.openVPNConfigDataBase64);
+//       final config = utf8.decode(data);
+//       final vpnConfig = VpnConfig(
+//         country: vpn.value.countryLong,
+//         username: 'vpn',
+//         password: 'vpn',
+//         config: config,
+//       );
+//
+//       // Code to initiate the VPN connection with vpnConfig
+//       Color get getButtonColor {
+//         switch (vpnState.value) {
+//           case VpnEngine.vpnDisconnected:
+//             return Colors.blue;
+//           case VpnEngine.vpnConnected:
+//             return Colors.green;
+//           default:
+//             return Colors.orangeAccent;
+//         }
+//       }
+//
+//       // VPN button text based on state
+//       String get getButtonText {
+//         switch (vpnState.value) {
+//           case VpnEngine.vpnDisconnected:
+//             return 'Tap to Connect';
+//           case VpnEngine.vpnConnected:
+//             return 'Disconnect';
+//           default:
+//             return 'Connecting...';
+//         }
+//       }
+//       await VpnEngine.startVpn(vpnConfig);
+//
+//     } else {
+//       // await VpnEngine.stopVpn();
+//     }
+//
+//     // Code to show interstitial ad and then connect to VPN
+//   }
+//
+//   // VPN button color based on state
+//
+// }
 
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../helper/my_dialog.dart';
@@ -10,7 +75,6 @@ import '../services/vpn_engine.dart';
 
 class HomeController extends GetxController {
   final Rx<Vpn> vpn = Pref.vpn.obs;
-
   final vpnState = VpnEngine.vpnDisconnected.obs;
 
   void connectToVpn() async {
@@ -20,25 +84,27 @@ class HomeController extends GetxController {
     }
 
     if (vpnState.value == VpnEngine.vpnDisconnected) {
-      // log('\nBefore: ${vpn.value.openVPNConfigDataBase64}');
-
-      final data = Base64Decoder().convert(vpn.value.openVPNConfigDataBase64);
-      final config = Utf8Decoder().convert(data);
+      final data = base64.decode(vpn.value.openVPNConfigDataBase64);
+      final config = utf8.decode(data);
       final vpnConfig = VpnConfig(
-          country: vpn.value.countryLong,
-          username: 'vpn',
-          password: 'vpn',
-          config: config);
+        country: vpn.value.countryLong,
+        username: 'vpn',
+        password: 'vpn',
+        config: config,
+      );
 
-      // log('\nAfter: $config');
+      vpnState.value = VpnEngine.vpnConnecting;
+      await VpnEngine.startVpn(vpnConfig);
+      vpnState.value = VpnEngine.vpnConnected;
 
-      //code to show interstitial ad and then connect to vpn
-      // AdHelper.showInterstitialAd(onComplete: () async {
-      //   await VpnEngine.startVpn(vpnConfig);
-      // });
     } else {
+      // vpnState.value = VpnEngine.vpnDisconnecting;
       await VpnEngine.stopVpn();
+      vpnState.value = VpnEngine.vpnDisconnected;
     }
+
+    // Code to show interstitial ad and then connect to VPN
+
   }
 
   // vpn buttons color
